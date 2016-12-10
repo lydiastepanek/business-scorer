fade96aed1cb0539ac777111e9b7522a174a5647
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-12-10 16:16:03 -0500

    make request to google places details

--- src/google2.py
+++ src/google2.py
@@ -8,15 +8,16 @@ import pprint
 #  DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJOwg_06VPwokRYv534QaPC8g&key=AIzaSyDa2cfifSiwMIDSBRWRENhVej5fbuzfweg"
 #  URL2 = "http://maps.googleapis.com/maps/api/geocode/json?address=New+York&sensor=false"
 #  f = { 'address' : 'Oxford University, uk', 'sensor' : "false"}
+#  pprint.pprint(jsonResponse['results'][0]['place_id'])
 
 f = { 'address' : '368 Graham Ave, Brooklyn, NY 11211', 'sensor' : "false"}
-print urllib.urlencode(f)
-
 URL = "https://maps.googleapis.com/maps/api/geocode/json?" + urllib.urlencode(f)
 
 googleResponse = urllib.urlopen(URL)
 jsonResponse = json.loads(googleResponse.read())
-#  pprint.pprint(jsonResponse)
-print pprint.pprint(jsonResponse['results'])
+place_id = jsonResponse['results'][0]['place_id']
 
-
+DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place_id + "&key=AIzaSyDa2cfifSiwMIDSBRWRENhVej5fbuzfweg"
+googleResponse = urllib.urlopen(DETAILS_URL)
+jsonResponse = json.loads(googleResponse.read())
+print pprint.pprint(jsonResponse)
