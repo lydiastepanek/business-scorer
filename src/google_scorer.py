dfd54d9d2906b0253dc4286cf21a425cba1ba293
Lydia Stepanek <lydia.stepanek@gmail.com>
2016-12-11 11:07:21 -0500

    refactor google scorer

--- src/google_scorer.py
+++ src/google_scorer.py
@@ -1,11 +1,14 @@
 import urllib, json
 
 def run(business_name, business_address):
+    json_response = get_business_details(business_name, business_address)
+    rating = json_response['rating'] if json_response['rating'] else 0
+    business_categories = len(json_response['types']) if json_response['types'] else 0
+    total_rating = (float(rating) / 5) + (.05 * business_categories)
+    return min(total_rating, 1)
+
+def get_business_details(business_name, business_address):
     params = { "query" : business_name + " " + business_address }
     URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?" + urllib.urlencode(params) + "&key=AIzaSyDa2cfifSiwMIDSBRWRENhVej5fbuzfweg"
     googleResponse = urllib.urlopen(URL)
-    jsonResponse = json.loads(googleResponse.read())['results'][0]
-    rating = jsonResponse['rating'] if jsonResponse['rating'] else 0
-    business_categories = len(jsonResponse['types']) if jsonResponse['types'] else 0
-    total_rating = (float(rating) / 5) + (.05 * business_categories)
-    return min(total_rating, 1)
+    return json.loads(googleResponse.read())['results'][0]
